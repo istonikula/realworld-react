b4ccc9e0abf466311214372f670ac07559cbb0da
Isto Nikula <isto.nikula@iki.fi>
2024-01-07 14:07:29 +0200

    routing: add wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,59 @@
+import { Link, Route, Switch } from 'wouter'
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="/">conduit</a>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Sign in</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Sign up</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/">
+            <div>
+              <h1 className="text-3xl font-bold underline">
+                A place to share your knowledge.
+              </h1>
+            </div>
+          </Route>
+          <Route path="/login">
+            <div>
+              <h1 className="text-3xl font-bold underline">
+                Sign in
+              </h1>
+            </div>
+          </Route>
+          <Route path="/register">
+            <div>
+              <h1 className="text-3xl font-bold underline">
+                Sign up
+              </h1>
+            </div>
+          </Route>
+        </Switch>
+      </main>
+      <footer>
+        <div className="container">
+          <Link to="/" className="logo-font">conduit</Link>
+          <span className="attribution">
+            An interactive learning project from <a href="https://thinkster.io">Thinkster</a>.
+            Code &amp; design licensed under MIT.
+          </span>
+        </div>
+      </footer>
+    </>
   )
 }
  export function helloVitest() {
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 import './index.css'
+import './bs.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
