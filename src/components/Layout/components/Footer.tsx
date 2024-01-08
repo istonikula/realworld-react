0049fac8acfbff5f5ac84f6632685db1c0145cbf
Isto Nikula <isto.nikula@iki.fi>
2024-01-08 09:17:37 +0200

    styles: remove attribution

--- src/components/Layout/components/Footer.tsx
+++ src/components/Layout/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer>
       <div className="container mx-auto px-3">
         <Link to="/" className="align-middle font-['Titillium_Web']">conduit</Link>
-        <span className="attribution">
+        <span className="ml-2.5 align-middle text-xs text-[#bbb] font-light">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>.
           Code &amp; design licensed under MIT.
         </span>
