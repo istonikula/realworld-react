41868dfbe091c991d781861947e0e61bf2188573
Isto Nikula <isto.nikula@iki.fi>
2024-01-08 11:11:53 +0200

    styles: bs.css, remove obsolete classes
    Remove css classes not used in official templates: https://www.realworld.how/docs/specs/frontend-specs/templates

--- src/components/Layout/components/Footer.tsx
+++ src/components/Layout/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from 'wouter'
 
 export function Footer() {
   return (
-    <footer className="absolute bottom-0 mt-12 py-4 w-full bg-[#f3f3f3] ">
+    <footer className="absolute bottom-0 mt-12 py-4 w-full bg-[#f3f3f3]">
       <div className="container mx-auto px-3 flex items-center">
         <Link to="/" className="font-['Titillium_Web']">conduit</Link>
         <span className="ml-2.5 text-xs text-[#bbb] font-light">
