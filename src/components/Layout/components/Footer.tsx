b132af2ddaf2e2553ab03a919a22cef0b02e0037
Isto Nikula <isto.nikula@iki.fi>
2024-01-08 10:36:27 +0200

    styles: footer, center items using flex

--- src/components/Layout/components/Footer.tsx
+++ src/components/Layout/components/Footer.tsx
@@ -3,9 +3,9 @@ import { Link } from 'wouter'
 export function Footer() {
   return (
     <footer className="absolute bottom-0 mt-12 py-4 w-full bg-[#f3f3f3] ">
-      <div className="container mx-auto px-3">
-        <Link to="/" className="align-middle font-['Titillium_Web']">conduit</Link>
-        <span className="ml-2.5 align-middle text-xs text-[#bbb] font-light">
+      <div className="container mx-auto px-3 flex items-center">
+        <Link to="/" className="font-['Titillium_Web']">conduit</Link>
+        <span className="ml-2.5 text-xs text-[#bbb] font-light">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>.
           Code &amp; design licensed under MIT.
         </span>
