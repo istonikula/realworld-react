f4e4a003c2d2117f48de1feac8c2b2b26070b8e7
Isto Nikula <isto.nikula@iki.fi>
2024-01-09 18:11:26 +0200

    styles: style home page banner using tw

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
+import { cx } from 'class-variance-authority'
+
 export function HomePage() {
   return (
     <div className="home-page">
-      <div className="banner">
+      <div className={cx(
+        '[box-shadow:inset_0_8px_8px_-8px_rgba(0,_0,_0,_0.3),_inset_0_-8px_8px_-8px_rgba(0,_0,_0,_0.3)]',
+        'mb-8 p-8 text-white bg-[#5cb85c]'
+      )}>
         <div className="container mx-auto px-3">
-          <h1 className="font-['Titillium_Web']">conduit</h1>
-          <p>A place to share your knowledge.</p>
+          <h1 className={cx(
+            '[text-shadow:0px_1px_3px_rgba(0,_0,_0,_0.3)]',
+            'pb-2 font-[\'Titillium_Web\'] text-center text-[3.5rem] leading-[1.1]'
+          )}>
+            conduit
+          </h1>
+          <p className="font-light text-center text-2xl">A place to share your knowledge.</p>
         </div>
       </div>
     </div>
