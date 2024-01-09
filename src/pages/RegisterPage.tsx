228f8b34f9a017b8aab595d8d5a3701499f21306
Isto Nikula <isto.nikula@iki.fi>
2024-01-10 00:19:54 +0200

    styles: style register page with tw

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,33 +1,54 @@
+import { cx } from 'class-variance-authority'
+
 export function RegisterPage() {
   return (
-    <div className="mt-6">
-      <div className="container mx-auto px-3">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign up</h1>
-            <p className="text-xs-center">
-              <a href="/login">Have an account?</a>
-            </p>
+    <div className="container mt-6 mx-auto px-6">
+      <div className={cx([
+        'mx-auto max-w-md',
+        'sm:w-3/5 md:w-1/2',
+        'flex flex-col'
+      ])}>
+        <h1 className="mb-2 self-center text-[2.5rem] leading-[1.1]">Sign up</h1>
+        <p className="mb-4 self-center">
+          <a href="/register">Have an account?</a>
+        </p>
 
-            <ul className="error-messages">
-              <li>That email is already taken</li>
-            </ul>
+        <ul className="mb-4 text-[#b85c5c] font-bold">
+          <li>That email is already taken</li>
+        </ul>
 
-            <form>
-              <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Username" />
-              </fieldset>
-              <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Email" />
-              </fieldset>
-              <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="password" placeholder="Password" />
-              </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
-            </form>
-          </div>
-        </div>
+        <form className="flex flex-col">
+          <fieldset className="mb-4">
+            <input className={formControl} type="text" placeholder="Username" />
+          </fieldset>
+          <fieldset className="mb-4">
+            <input className={formControl} type="text" placeholder="Email" />
+          </fieldset>
+          <fieldset className="mb-4">
+            <input className={formControl} type="password" placeholder="Password" />
+          </fieldset>
+          <button className={btn}>Sign up</button>
+        </form>
       </div>
     </div>
   )
 }
+
+const formControl = cx([
+  'w-full',
+  'border border-solid border-black/15 rounded',
+  'focus:border-[#66afe9] focus:outline-none',
+  'py-3 px-6',
+  'text-xl/none',
+])
+
+const btn = cx([
+  'self-end',
+  'rounded',
+  'py-3 px-6',
+  'text-xl/5',
+  'text-white',
+  'bg-[#5cb85c]',
+  'hover:bg-[#419641] hover:border-[#449d44]',
+  'focus:bg-[#419641]'
+])
