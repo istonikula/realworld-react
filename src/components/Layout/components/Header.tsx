59654a09a140a21f17ad563ebd6eed161fa55855
Isto Nikula <isto.nikula@iki.fi>
2024-01-09 14:17:38 +0200

    styles: style header using tw

--- src/components/Layout/components/Header.tsx
+++ src/components/Layout/components/Header.tsx
@@ -1,22 +1,49 @@
-import { Link } from 'wouter'
+import { cva } from 'class-variance-authority'
+import { Link as WLink, useRoute } from 'wouter'
 
 export function Header() {
   return (
-    <nav className="navbar navbar-light">
-      <div className="container mx-auto px-3">
-        <a className="navbar-brand" href="/">conduit</a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <Link className="nav-link active" to="/">Home</Link>
+    <nav className="py-2 px-4">
+      <div className="container mx-auto px-3 flex justify-between items-center">
+        <a className="font-['Titillium_Web'] text-2xl" href="/">conduit</a>
+        <ul className="flex">
+          <li>
+            <Link to="/">Home</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">Sign in</Link>
+          <li className="ml-4">
+            <Link to="/login">Sign in</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">Sign up</Link>
+          <li className="ml-4">
+            <Link to="/register">Sign up</Link>
           </li>
         </ul>
       </div>
     </nav>
   )
 }
+
+function Link(props: React.PropsWithChildren<{ to: string }>) {
+  const [active] = useRoute(props.to)
+  const cn = link({ state: active ? 'active' : 'inactive' })
+  return (<WLink className={cn} to={props.to}>{props.children}</WLink>)
+}
+const link = cva(
+  [
+    'hover:no-underline',
+    'focus:no-underline'
+  ], {
+  variants: {
+    state: {
+      active: [
+        'text-black/80',
+        'hover:text-black/80',
+        'focus:text-black/80'
+      ],
+      inactive: [
+        'text-black/30',
+        'hover:text-black/60',
+        'focus:text-black/60'
+      ]
+    }
+  }
+})
