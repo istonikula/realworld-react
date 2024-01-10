558b509a55972b269ff3475db6e9e1ff79cb8c42
Isto Nikula <isto.nikula@iki.fi>
2024-01-10 10:07:15 +0200

    register: form, first draft

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,6 +1,27 @@
 import { cx } from 'class-variance-authority'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup
+  .object({
+    username: yup.string().min(5).required().label('Username'),
+    email: yup.string().email().required().label('Email'),
+    password: yup.string().min(5).required().label('Password'),
+  })
+  .required()
+type Inputs = yup.InferType<typeof schema>
 
 export function RegisterPage() {
+  const { register, handleSubmit, formState: { errors } } = useForm({ 
+    mode: 'onTouched',
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log('submit', data)
+  }
+
   return (
     <div className="mt-6 px-6">
       <div className={cx([
@@ -13,20 +34,23 @@ export function RegisterPage() {
         </p>
 
         <ul className="mb-4 text-[#b85c5c] font-bold">
-          <li>That email is already taken</li>
+          <li>{errors.username?.message}</li>
+          <li>{errors.email?.message}</li>
+          <li>{errors.password?.message}</li>
         </ul>
 
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={void handleSubmit(onSubmit)}>
           <fieldset className="mb-4">
-            <input className={formControl} type="text" placeholder="Username" />
+            <input className={formControl} type="text" placeholder="Username" {...register('username')} />
           </fieldset>
           <fieldset className="mb-4">
-            <input className={formControl} type="text" placeholder="Email" />
+            <input className={formControl} type="text" placeholder="Email" {...register('email')} />
           </fieldset>
           <fieldset className="mb-4">
-            <input className={formControl} type="password" placeholder="Password" />
+            <input className={formControl} type="password" placeholder="Password" {...register('password')} />
           </fieldset>
-          <button className={btn}>Sign up</button>
+
+          <button className={btn} type="submit">Sign up</button>
         </form>
       </div>
     </div>
